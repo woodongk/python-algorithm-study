bf5f0b65f4e23b45c36de3b5765eea8e757878f2
woodong94 <gks3284@naver.com>
2020-09-26 23:43:26 +0900

    twoSum resolve

--- twoSum.py
+++ twoSum.py
@@ -1,12 +1,12 @@
 from typing import List
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i,n_i in enumerate(nums):
-            for j,n_j in enumerate(nums):
-                if ((n_i+n_j) == target):
-                    return [i,j]
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if (nums[i] + nums[j]) == target:
+                return [i,j]
 
 if __name__ == '__main__':
-    sol = Solution()
-    print(sol.twoSum([3,2,4], 6))
+    print(twoSum([3,2,4], 6)) # [1,2]
+    print(twoSum([3,3], 6)) # [0,1]
