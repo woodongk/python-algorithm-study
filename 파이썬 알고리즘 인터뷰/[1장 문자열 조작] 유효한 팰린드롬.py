e7e18e5b108bd40a5034e52f293e9bee61efecc4
woodong94 <gks3284@naver.com>
2021-03-01 21:22:30 +0900

    solve 1-1

---
+++ 파이썬 알고리즘 인터뷰/[1장 문자열 조작] 유효한 팰린드롬.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # 알파벳과 숫자만 사용하니까......
+        alphabets = [chr(c).lower() for c in range(65, 65 + 26)]
+        numbers = [str(i) for i in range(0, 10)]
+
+        clean_s = s.lower()
+        clean_s = [c for c in clean_s if c in alphabets + numbers]
+
+        if clean_s == clean_s[::-1]:
+            return True
+        else:
+            False
+
+
+if __name__ == '__main__':
+    string = "A man, a plan, a canal: Panama"
+
+    sol = Solution()
+
+    print(sol.isPalindrome(string))
