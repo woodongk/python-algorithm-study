46ae76466d0aa6f744e81bfccf8cd1b91bdae0c0
woodong94 <gks3284@naver.com>
2021-03-18 12:14:07 +0900

    upload [4-1]

---
+++ 이것이 취업을 위한 코딩테스트다/04 구현/[4-1] 상하좌우.py
@@ -0,0 +1,35 @@
+"""
+5
+R R R U D D
+"""
+
+if __name__ == '__main__':
+
+    # 1. input 받기
+    N = int(input())
+    directions = list(map(str, input().split()))
+
+    # # 2. 행렬 만들기
+    # maps = [[(i, j) for j in range(1, N + 1)] for i in range(1, N + 1)]
+    # print(maps)
+
+    di = {
+         'D' : (1, 0), # L
+         'U' : (-1, 0), # R
+         'R': (0, 1), # U
+         'L': (0, -1) # D
+    }
+
+    x = y = 1
+
+    # directions 순서대로 서칭
+    for d in directions:
+        dx, dy = di[d]
+        # 공간 벗어나는 경우 무시한다.
+        if (1 <= x + dx <= N) and (1 <= y + dy <= N):
+            nx = x + dx
+            ny = y + dy
+        # 이동!
+        x, y = nx, ny
+
+    print(x, y)
