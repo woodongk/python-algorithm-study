6cde648d4e8d0517c6b37468d302f8a8d84a4f38
woodong94 <gks3284@naver.com>
2021-03-27 02:57:42 +0900

    solve 1018

---
+++ BOJ/1018 체스판 다시 칠하기.py
@@ -0,0 +1,120 @@
+"""
+문제
+지민이는 자신의 저택에서 MN개의 단위 정사각형으로 나누어져 있는 M*N 크기의 보드를 찾았다.
+어떤 정사각형은 검은색으로 칠해져 있고, 나머지는 흰색으로 칠해져 있다.
+지민이는 이 보드를 잘라서 8*8 크기의 체스판으로 만들려고 한다.
+
+체스판은 검은색과 흰색이 번갈아서 칠해져 있어야 한다.
+구체적으로, 각 칸이 검은색과 흰색 중 하나로 색칠되어 있고, 변을 공유하는 두 개의 사각형은 다른 색으로 칠해져 있어야 한다.
+따라서 이 정의를 따르면 체스판을 색칠하는 경우는 두 가지뿐이다.
+하나는 맨 왼쪽 위 칸이 흰색인 경우, 하나는 검은색인 경우이다.
+
+보드가 체스판처럼 칠해져 있다는 보장이 없어서, 지민이는 8*8 크기의 체스판으로 잘라낸 후에 몇 개의 정사각형을 다시 칠해야겠다고 생각했다.
+당연히 8*8 크기는 아무데서나 골라도 된다. 지민이가 다시 칠해야 하는 정사각형의 최소 개수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N과 M이 주어진다. N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수이다.
+둘째 줄부터 N개의 줄에는 보드의 각 행의 상태가 주어진다. B는 검은색이며, W는 흰색이다.
+
+출력
+첫째 줄에 지민이가 다시 칠해야 하는 정사각형 개수의 최솟값을 출력한다.
+
+예제 입력 1
+8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+예제 출력 1
+1
+예제 입력 2
+10 13
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+WWWWWWWWWWBWB
+WWWWWWWWWWBWB
+예제 출력 2
+12
+"""
+import sys
+input = sys.stdin.readline
+
+# w가 처음, B가 처음일 경우 두 가지 존재한다.
+WB = [
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW"
+]
+BW = [
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+]
+
+
+# x : start x 좌표, y : start y 좌표
+def check_WB(x, y):
+    cnt = 0
+    for i in range(8):
+        for j in range(8):
+            if maps[x + i][y + j] != WB[i][j]:
+                cnt += 1
+
+    return cnt
+
+
+# x : start x 좌표, y : start y 좌표
+def check_BW(x, y):
+    cnt = 0
+    for i in range(8):
+        for j in range(8):
+            if maps[x + i][y + j] != BW[i][j]:
+                cnt += 1
+
+    return cnt
+
+
+if __name__ == '__main__':
+    # input 받기
+    n, m = map(int, input().split())
+
+    maps = []
+    for i in range(n):
+        maps.append(input()[:-1])
+    print(maps)
+
+    # 좌표를 sliding window 하여 각각의 경우에 대해서 최소값 계산하기
+    min_cnt = 1000
+    for x in range(n - 8 + 1):
+        for y in range(m - 8 + 1):
+            WB_cnt = check_WB(x,y)
+            BW_cnt = check_BW(x,y)
+            min_cnt = min([min_cnt, WB_cnt, BW_cnt])
+
+    print(min_cnt)
+
+
+
+
+
+
