df51e63d3cb376662a0acca1f0746b54f4f36998
woodong94 <gks3284@naver.com>
2021-04-08 10:51:26 +0900

    전화번호 목록 보충

--- Programmers/전화번호 목록.py
+++ Programmers/전화번호 목록.py
@@ -1,22 +1,37 @@
+# def solution(phone_book):
+#     phone_book = sorted(phone_book, key=lambda x: x[0])
+#
+#     prefix = phone_book[0] # 최솟값을 prefix로 설정한다.
+#     phone_book = phone_book[1:]  # 최솟값 제외한 전화번호 목록 갱신한다.
+#
+#     while len(phone_book) > 0:
+#         for p in phone_book:
+#             if str(p).startswith(prefix):
+#                 return False
+#             else:
+#                 continue
+#
+#         # 발견되지 않았다면 prefix와 phone_book 갱신해준다.
+#         prefix = phone_book[0]
+#         phone_book = phone_book[1:]
+#
+#     return True
+
+
 def solution(phone_book):
-    phone_book = sorted(phone_book, key=len)
+    # 예외 처리
+    if len(phone_book) == 1:
+        return False
 
-    prefix = phone_book[0] # 최솟값을 prefix로 설정한다.
-    phone_book = phone_book[1:]  # 최솟값 제외한 전화번호 목록 갱신한다.
+    phone_book = sorted(phone_book, key=lambda x: x[0])
 
-    while len(phone_book) > 1:
-        for p in phone_book:
-            if prefix == p[:len(prefix)]:
-                return False
-            else:
-                continue
+    for i in range(len(phone_book) - 1):
+        prefix = phone_book[i]
+        print(prefix)
+        if prefix == phone_book[i + 1][:len(prefix)]:
+            return False
 
-        # 발견되지 않았다면 prefix와 phone_book 갱신해준다.
-        prefix = phone_book[0]
-        phone_book = phone_book[1:]
-
-    answer = True
-    return answer
+    return True
 
 
 if __name__ == '__main__':
